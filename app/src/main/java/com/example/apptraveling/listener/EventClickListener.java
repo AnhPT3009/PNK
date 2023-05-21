f2f24a996869f39f56a16e664fce8179f473cb1a
Your Name <21012735@st.phenikaa-uni.edu.vn>
2023-05-21 20:59:15 +0700

    ...

--- app/src/main/java/com/example/apptraveling/listener/EventClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.apptraveling.listener;
-
-import com.example.apptraveling.models.Sk;
-
-public interface EventClickListener {
-    void onPopularClick(Sk sk);
-}
--- app/src/main/java/com/example/apptraveling/listener/EventListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.apptraveling.listener;
-
-import com.example.apptraveling.models.Event;
-
-public interface EventListener {
-    void onEventClick(Event event);
-}
